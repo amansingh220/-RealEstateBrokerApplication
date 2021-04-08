92fa3389f04bcff692375f258189122c4f84c1dd
Anjanimehta <anjanimehta99@gmail.com>
2021-04-08 15:41:30 +0530

    new commit

--- src/components/EditBroker.js
+++ src/components/EditBroker.js
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+
 import React ,{useState ,useEffect} from 'react'
 import { connect } from 'react-redux'
 import { updateBroker } from '../redux/Index'
